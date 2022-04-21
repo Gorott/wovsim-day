7a946a2de3dbeb5f8f7f3aceadbfa83a5a0dd388
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2022-04-21 22:31:34 +0800

    Update alchemists.js

--- kills/alchemists.js
+++ kills/alchemists.js
@@ -1,5 +1,5 @@
 const db = require("quick.db") // databas
-const { getRole, getEmoji } = require("../../../config")
+const { getRole, getEmoji } = require("../../../config") // funcions
 
 module.exports = async client => {
   const guild = client.guilds.cache.get("890234659965898813") // get the guild object - Object 
@@ -10,24 +10,28 @@ module.exports = async client => {
   const deadPlayers = players.filter(p => !alivePlayers.includes(p)) // get the dead players array - Array<Snowflake> c
   const alchemists = alivePlayers.filter(p => db.get(`player_${p}`).role === "Alchemist") // get the alive alchemist array - Array<Snowflake>
   
-  for (let alchemist in alchemists) {
-    let attacker = db.get(`player_${alchemist}`)
-        if (attacker.status === "Alive") {
-    if (attacker.blackTarget) {
+  // loop through every alive player
+  alivePlayers.forEach(async player => {
+    
+    let guy = db.get(`player_${player}`) // get the player object - Object
+  
+    // check if the player is poisoned
+    if (Array.isArray(guy.poisoned)) { // checks if they property is an array
       
-      let guy = db.get(`player_${attacker.blackTarget}`)
-      
-      if (guy.status === "Alive") {
-            db.set(`player_${guy.id}.status`, "Dead") 
-            
-            let attackedPlayer = await guild.members.fetch(guy.id) // fetch the discord member - Object 
-            let  attackedPlayerRoles = attackedPlayer.roles.cache.map(r => r.name === "Alive" ? "892046207428476989": r.id) // get all the roles and replace the Alive role with Dead. 
-            await dayChat.send(`${getEmoji("blackp", client)} The Alchemist killed **${players.indexOf(attackedPlayer.id)+1} ${attackedPlayer.username} (${getEmoji(guy.role?.toLowerCase()?.replace(/\s/g, "_"), client)} ${guy.role})**!`) 
-            await attackedPlayer.roles.set(attackedPlayerRoles)
+      // check if at least one of their attackers is alive
+      if (guy.poisoned.some(p => alivePlayers.includes(p))) { // checks if at least 1 alchemist who poisoned them is alive
         
-      }
-      
+        // kill the player
+        let member = await guild.members.fetch(guy.id) // fetch the discord member - Object
+        let memberRoles = member.roles.cache.map(r => r.name === "Alive" ? "892046207428476989" : r.id) // get all the player's roles - Array<Snowflake>
+        db.set(`player_${guy.id}.status`, "Dead") // change the status of the player
+        member.roles.set(memberRoles) // set the roles
+        dayChat.send(`${getEmoji("blackp", client)} The Alchemist killed **${guy.username} ${players.indexOf(guy.id)} (${getEmoji(guy.role.toLowerCase().replace(/\s/g, "_"), client)} ${guy.role})**!`) // send the message
+        
+      } 
+    
     }
-    }
-  }
+    
+  })
+  
 }
