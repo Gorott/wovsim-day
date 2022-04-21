b1c91856b0453b6edf7a271ed16ec63d945f410a
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2022-04-21 22:55:34 +0800

    Update bombers.js

--- kills/bombers.js
+++ kills/bombers.js
@@ -10,28 +10,37 @@ module.exports = async client => {
   const deadPlayers = players.filter(p => !alivePlayers.includes(p)) // get the dead players array - Array<Snowflake> c
   const bombers = alivePlayers.filter(p => db.get(`player_${p}`).role === "Bomber") // get the alive bombers array - Array<Snowflake>
   
-  for (bomber in bombers) {
-    let attacker = db.get(`player_${bomber}`)
+  // loop through each bomber
+  for (let bomber in bombers) {
     
+    let attacker = db.get(`player_${bomber}`) // get the attacker player object - Object
+    
+    // check if the attacker is alive
     if (attacker.status === "Alive") {
       
+      // check if the attacker has set their bombs
       if (attacker.target) {
         
-        if (attacker.setBombs == true) {
-          setTimeout( async function() {
+        // send this after 1 minute
+        setTimeout(async () => {
+          
+          // loop through each target
           attacker.target.forEach(async target => {
-            let guy = db.get(`player_${target}`) 
-            let attackedPlayer = await guild.members.fetch(guy.id) // fetch the discord member - Object 
             
+            let guy = db.get(`player_${target}`) // get the guy player - Object
+
+            // check if the guy is alive
             if (guy.status === "Alive") {
-              let attackedPlayerRoles = attackedPlayer.roles.cache.map(r => r.name === "Alive" ? "892046207428476989": r.id)
-              dayChat.send(`${getEmoji("explode", client)}  **${players.indexOf(guy.id)+1} ${guy.username} (${getEmoji(guy.role?.toLowerCase()?.replace(/\s/g, "_"), client)} ${guy.role})* was killed by an explosion!`)
-              await attackedPlayer.roles.set(attackedPlayerRoles)
-              db.set(`player_${guy.id}.status`, "Dead")
+              let attackedPlayer = await guild.members.fetch(guy.id) // fetch the discord member - Object 
+              let attackedPlayerRoles = attackedPlayer.roles.cache.map(r => r.name === "Alive" ? "892046207428476989": r.id) // get the member roles - Array<Snowflake>
+              await attackedPlayer.roles.set(attackedPlayerRoles) // set the roles
+              db.set(`player_${guy.id}.status`, "Dead") // set the status of the player
+              dayChat.send(`${getEmoji("explode", client)}  **${players.indexOf(guy.id)+1} ${guy.username} (${getEmoji(guy.role?.toLowerCase()?.replace(/\s/g, "_"), client)} ${guy.role})** was killed by an explosion!`) // send the message
+              
             }
           })
-          }, 60000)
-        }
+        }, 60000)
+        
       }
     }
   }
