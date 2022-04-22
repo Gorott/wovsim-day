b79e98ce3b56fe57551b60c95f5ddf01566a1cb8
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2022-04-22 21:57:43 +0800

    Update jailers.js

--- others/jailers.js
+++ others/jailers.js
@@ -5,35 +5,52 @@ module.exports = async client => {
   const werewolvesChat = guild.channels.cache.find(c => c.name === "werewolves-chat") // get the werewolves channel - Object 
   const players = db.get(`players`) || [] // get the players array - Array<Snowflake> 
   const alivePlayers = players.filter(p => db.get(`player_${p}`).status === "Alive") // get the alive players array - Array<Snowflake> 
-  const deadPlayers = players.filter(p => !alivePlayers.includes(p)) // get the dead players array - Array<Snowflake> c
+  const deadPlayers = players.filter(p => !alivePlayers.includes(p)) // get the dead players array - Array<Snowflake>
   const jailers = alivePlayers.filter(p => db.get(`player_${p}`).role === "Jailer") // get the alive jailers array - Array<Snowflake>
   
+  // loop through each jailer
   for (let jailer in jailers) {
-    let jail = db.get(`player_${jailer}`)
     
+    let jail = db.get(`player_${jailer}`) // get the jailer object - Object
+    
+    // check if the jailer is alive
     if (jail.status === "Alive") {
       
+      // check if the jailer has selected a target
       if (jail.target) {
-        let guy = db.get(`player_${jail.target}`)
-        let channel = guild.channels.cache.get(guy.channel)
-        let player = await guild.members.fetch(guy.id)
-        if (guy.status === "Alive") {
-        channel.permissionOverwrites.edit(guy.id, { 
-        SEND_MESSAGES: false, 
-        VIEW_CHANNEL: true, 
-        READ_MESSAGE_HISTORY: true 
-      })
         
-        jailedChat.permissionOverwrites.edit(guy.id, {
-          SEND_MESSAGES: true, 
-          VIEW_CHANNEL: true, 
-          READ_MESSAGE_HISTORY: true 
-        })
-        if (guy.team === "Werewolf") {
-              wwChat.send("Your werewolf teammate **" + players.indexOf(player.id)+1 + player.username + " (" + getEmoji(guy.role?.toLowerCase()?.replace(/\s/g, "_"), client) + guy.role + ")** has been jailed!")
-        }
-        chan.send(`You have been jailed! You can't do your actions for tonight! Head to <#${jailedChat.id}> to talk with the jailer!`)
-        db.set(`player_${guy.id}.jailed`, true)
+        let guy = db.get(`player_${jail.target}`) // get the player to be jailed - Object
+        let channel = guild.channels.cache.get(guy.channel) // get their channel - Object
+        let player = await guild.members.fetch(guy.id) // fetch the discord member - Promise<Object>
+        
+        // check if the player is alive
+        if (guy.status === "Alive") { 
+          
+          // change the channel's permissions
+          channel.permissionOverwrites.edit(guy.id, { 
+            SEND_MESSAGES: false, 
+            VIEW_CHANNEL: true, 
+            READ_MESSAGE_HISTORY: true 
+          })
+        
+          // give access to jailedchat
+          jailedChat.permissionOverwrites.edit(guy.id, {
+            SEND_MESSAGES: true, 
+            VIEW_CHANNEL: true, 
+            READ_MESSAGE_HISTORY: true 
+          })
+          
+          // check if they belong to the Werewolf team  
+          if (guy.team === "Werewolf" && guy.role !== "Werewolf Fan") {
+            
+            // send the message 
+            werewolvesChat.send(`Your werewolf teammate **${players.indexOf(player.id)+1} ${player.username} (${getEmoji(guy.role?.toLowerCase()?.replace(/\s/g, "_"), client)} ${guy.role}** has been jailed!`)
+            
+          }
+          
+          
+          channel.send(`You have been jailed! You can't do your actions for tonight! Head to <#${jailedChat.id}> to talk with the jailer!`) // send the message
+          db.set(`player_${guy.id}.jailed`, true) // set the jailed status to true
         }
       }
     }
